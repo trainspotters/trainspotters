c15f3f1a5cd9b77f043e8b934241e940a69354c6
Victor Jolissaint <victor.jolissaint@gmail.com>
2015-12-06 00:05:32 +0000

    Add all the stations on a map

--- client/src/views/map/index.js
+++ client/src/views/map/index.js
@@ -4,12 +4,13 @@ import stations from '../../../../stations.json';
 
 export class MapStations extends Component {
   render() {
+    const position = [51.505, -0.09];
     const markers = stations.map((station) => {
-      return <Marker position={[station.lat, station.lng]}></Marker>;
+      return <Marker position={[station.lat, station.lng]} key={station.displayName}></Marker>;
     });
 
     return (
-      <Map style={{width: "300px", height: "300px"}} center={position} zoom={13}>
+      <Map style={{width: "600px", height: "300px"}} center={position} zoom={13}>
         <TileLayer
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
