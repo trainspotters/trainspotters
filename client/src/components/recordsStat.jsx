b64676b754350d50237a9a2df3957d22e3162dc6
Timur Abishev <tabishev@twitter.com>
2015-12-16 04:11:48 +0000

    add some additional stats

--- client/src/components/recordsStat.jsx
+++ client/src/components/recordsStat.jsx
@@ -1,8 +1,18 @@
 'use strict';
 import React from 'react';
-import { journeysTimeInHours } from '../recordsUtils.js'
+import { journeysTimeInHours, isLegalTwoSidedJourney } from '../recordsUtils.js'
+import { recordTypes } from 'trainspotters-parser';
 
-export const RecordsStat = ({records}) =>
-  (<div>
-    <div>Journeys cumulative duration: {journeysTimeInHours(records)} hours.</div>
+export const RecordsStat = ({records}) => {
+  const journeysTime = journeysTimeInHours(records);
+  const twoSideJourneysCount = records.filter(isLegalTwoSidedJourney).length;
+  const busJourneys = records
+    .filter((record) => record.type == recordTypes.BUS_JOURNEY).length;
+
+  return (<div>
+    <div>Journeys cumulative duration: {journeysTime} hours.</div>
+    <div>Journeys count (not including bus journeys): {twoSideJourneysCount}.</div>
+    <div>Average journey time: {(journeysTime/twoSideJourneysCount)*60} minutes.</div>
+    <div>Bus journeys count: {busJourneys}.</div>
   </div>);
+}
