84d8a5fb7c25adcadc7152cd455a53d15373d3fe
Victor Jolissaint <vjolissaint@twitter.com>
2015-12-18 19:08:29 +0000

    better formating for duration

--- client/src/components/recordsStat.jsx
+++ client/src/components/recordsStat.jsx
@@ -1,18 +1,22 @@
 'use strict';
 import React from 'react';
-import { journeysTimeInHours, isLegalTwoSidedJourney } from '../recordsUtils.js'
+import { journeysTimeInSeconds, isLegalTwoSidedJourney } from '../recordsUtils.js'
 import { recordTypes } from 'trainspotters-parser';
+import moment from 'moment';
+import 'moment-duration-format';
 
 export const RecordsStat = ({records}) => {
-  const journeysTime = journeysTimeInHours(records);
+  const totalSeconds = journeysTimeInSeconds(records);
+  const journeysTime = moment.duration(totalSeconds, 'seconds').format("h [hours and] m [minutes]");
   const twoSideJourneysCount = records.filter(isLegalTwoSidedJourney).length;
+  const avgJourneyTime = moment.duration(totalSeconds/twoSideJourneysCount, 'seconds').format("h [hours and] m [minutes]");
   const busJourneys = records
-    .filter((record) => record.type == recordTypes.BUS_JOURNEY).length;
+    .filter((record) => record.type === recordTypes.BUS_JOURNEY).length;
 
   return (<div>
-    <div>Journeys cumulative duration: {journeysTime} hours.</div>
+    <div>Journeys cumulative duration: {journeysTime}.</div>
+    <div>Average journey time: {avgJourneyTime}.</div>
     <div>Underground journeys count: {twoSideJourneysCount}.</div>
-    <div>Average journey time: {(journeysTime/twoSideJourneysCount)*60} minutes.</div>
     <div>Bus journeys count: {busJourneys}.</div>
   </div>);
 }
